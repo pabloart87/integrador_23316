827b9feb19d6d5bf9847f6cb0c4931f08c0c93b2
pabloart87 <pablo_ariel_toledo@hotmail.com>
2023-04-02 12:19:22 -0300

    agregando fibo

---
+++ fibo.py
@@ -0,0 +1,20 @@
+# Fibonacci numbers module
+ 
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+ 
+def fib2(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return print(result)
+
+fib(100)
+
+fib2(100)
